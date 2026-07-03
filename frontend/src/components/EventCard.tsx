b8bbb857e5cbd386999f50de018fece1ea63fed8
Ryan He <rha136@sfu.ca>
2026-07-02 23:57:21 -0700

    finished basic layout for the event cards to be displayed

--- frontend/src/components/EventCard.tsx
+++ frontend/src/components/EventCard.tsx
@@ -2,7 +2,15 @@ export default function EventCard(props: any){
     const event = props.event
     return(
         <>
-            <p>name = {event.title}</p>
+            <div>
+                <h3>{event.title}</h3>
+                <h4>location: {event.location}</h4>
+                <h4>date: {event.date}</h4>
+                <h4>category: {event.category}</h4>
+                <p>{event.description}</p>
+                <p>url: {event.url}</p>
+
+            </div>
         </>
     )
 
