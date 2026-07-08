74602a880705213ce2bcf3c6374f712f8dc8fd49
Ryan He <rha136@sfu.ca>
2026-07-08 16:06:40 -0700

    initalized Oauth2Client in googleAuth.ts

---
+++ backend/src/services/googleAuth.ts
@@ -0,0 +1,12 @@
+import { google } from 'googleapis'
+
+export const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  'http://localhost:3000/auth/google/callback'
+)
+
+export const authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: 'https://www.googleapis.com/auth/calendar'
+});
